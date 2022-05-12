1076b5d495672603909f632bf6e499bf262b221b
Gstars01 <khw4986@gmail.com>
2022-05-12 16:43:56 +0900

    Merge remote-tracking branch 'origin/master'

--- src/quick_sort.java
+++ src/quick_sort.java
@@ -1,5 +1,18 @@
 public class quick_sort {
+    public static int quick_sort(int[] arr){
+        int num = arr.length;
+        int pivot = arr[0];
+        int i = 0;
+        int j = num-1;
+        int[] ar1 = new int[]{};
+        int[] ar2 = new int[]{};
+        System.out.println(arr);
+        return 0;
+    }
     public static void main(String[] args) {
-        System.out.println("push");
+        System.out.println("quick sort");
+        int[] arr = new int[]{4,1,2,3,9,7,8,6,10,5};
+        quick_sort(arr);
+
     }
 }
