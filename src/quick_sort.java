6a2da64212544b9e48962e0c994a1eb7a9d2d317
Gstars01 <khw4986@gmail.com>
2022-05-16 19:38:40 +0900

    Merge remote-tracking branch 'origin/master'

--- src/quick_sort.java
+++ src/quick_sort.java
@@ -11,17 +11,17 @@ public class quick_sort {
         int left = start;
         int right = end;
         int[] memory = new int[]{};
-        while (left<right){
-            while(left<right && arr[left] < pivot){
-                left++;
+        while (left<right){     //left(start)가 right(end)보다 클동안만
+            while(left<right && arr[left] < pivot){     //arr[left(start)]가 pivot 보다 작거나 left(start)가 right(end)보다 작을때만 실행
+                left++;                                 //left 의 좌료를 오른쪽으로 한칸 이동
             }
-            while(right<left && arr[right] < pivot){
-                right++;
+            while(right<left && arr[right] < pivot){    //arr[right(end)]가 pivot 보다 크거나 right(end) left(start)보다 클동안만 실행
+                right--;                                //right 의 좌표를 왼쪽으로 한칸 이동
             }
             if(left <right) {
-                memory = change(arr[left], arr[right]);
-                memory[0] = arr[right];
-                memory[1] = arr[left];
+                memory = change(arr[left], arr[right]); //left(pivot 보다 큰수)와 right(pivot 보다 작은수 )를 교환
+                arr[right] = memory[0];                 //바꿔서 돌아온수를 각자 원래 위치로 이동
+                arr[left] = memory[1];
             }
 
         }
